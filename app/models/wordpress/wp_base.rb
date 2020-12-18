33438f0dc3eaa02e9cd107b7f9ca7e1dee408420
Julien Lestavel <julien@wecasa.fr>
2020-12-18 15:28:58 +0100

    Remove a warning in Rails >=6.1

--- app/models/wordpress/wp_base.rb
+++ app/models/wordpress/wp_base.rb
@@ -3,7 +3,12 @@ module Wordpress
     self.abstract_class = true
 
     def self.prefix_table_name table_name
-      prefix ||= self.connection_config[:prefix] || "wp"
+      prefix = if Rails::VERSION::MAJOR >= 6 && Rails::VERSION::MINOR >= 1
+                 connection_db_config.configuration_hash[:prefix]
+               else
+                 connection_config[:prefix]
+               end
+      prefix ||= 'wp'
       "#{prefix}_#{table_name}"
     end
   end
